87ccdbd9d5bb333b6ccb3eddf5df6c86e5653f78
marcelo <marcelo@marcelo.marcelo>
2017-10-10 20:59:46 -0300

    fix

---
+++ personas.pl
@@ -0,0 +1,33 @@
+person(ana, female).
+person(betty, female).
+person(carlos, male).
+person(diego, male).
+person(esther, female).
+person(franco, male).
+person(gaston, male).
+person(hector, male).
+person(ignacio, male).
+person(juana, female).
+person(kelly, female).
+person(luis, male).
+person(maria, female).
+person(natalia, female).
+person(oscar, male).
+person(pia, female).
+person(quique, male).
+person(raul, male).
+person(sofia, female).
+person(tomas, male).
+
+parents(betty, ana, diego).
+parents(carlos, ana, diego).
+parents(gaston, esther, franco).
+parents(hector, betty, gaston).
+parents(ignacio, betty, gaston).
+parents(luis, juana, ignacio).
+parents(maria, juana, ignacio).
+parents(natalia, kelly, hector).
+parents(pia, natalia, oscar).
+parents(raul, maria, quique).
+parents(sofia, maria, quique).
+parents(tomas, maria, quique).
